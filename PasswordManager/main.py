d8314752a1875792c5a76b0fb224441fea99add4
Erik Dio <erik.theboy@yahoo.com.br>
2025-01-21 13:52:08 -0300

    Changed pin to stenography

--- PasswordManager/main.py
+++ PasswordManager/main.py
@@ -1,144 +1,49 @@
-# Password Manager ----- Eye see you
-import os, sys
-import pyperclip
+from PIL import Image
+import secrets
+import tkinter as tk
+from tkinter import filedialog
 
-Show:bool = False
-Size:int = 16
-char_list:str = "abcdefghijklmnopqrstuvwxyz1234567890-+.,;/\\!@#$%&*()_=~{}[]"
+# Create the main window
+root = tk.Tk()
+root.title("Image Viewer")
+root.configure(bg='black')
 
+# Function to open an image file
+def open_image():
+    file_path = filedialog.askopenfilename(filetypes=[("Image Files", "*.png;*.jpg;*.jpeg")])
+    sz = 128
+    image = Image.open(file_path)
+    offset = int(str(image.width)[0]) + int(str(image.height)[0])
+    print(image.mode)
+    pin = f"{secrets.randbelow(10**128):0128d}"
+    print(pin)
+    pixel_lst = list(image.getdata())
+    for cursor in range(1, sz):
+        r=g=b=a=0
+        if image.mode == 'RGB':
+            r, g, b = pixel_lst[cursor*offset]
+        elif image.mode == 'RGBA':
+            r, g, b, a = pixel_lst[cursor*offset]
+        binario = bin(int(pin[cursor]))[2:].zfill(4)
+        print(binario)
+        r = int(f"{bin(r)[:-2]}" + binario[-2:], 2)
+        g = int(f"{bin(g)[:-2]}" + binario[-4:-2], 2)
+        b = int(f"{bin(b)[:-2]}" + binario[-6:-4], 2)
+        x = cursor
+        y = 0
+        while x > image.width:
+            x -= image.width
+            y += 1
+        if image.mode == 'RGB':
+            image.putpixel(xy=(x,y), value=(r, g, b))
+        elif image.mode == 'RGBA':
+            image.putpixel(xy=(x,y), value=(r, g, b, a))
+        cursor += 1
 
-# ---------------------- Main function
-def __main__():	
-	_isint = False
-	while(_isint == False):
-		try:
-			chave: str = input("Digite sua senha numérica. Qualquer número serve:\n*o número não será salvo, então é importante memorizar.\n")
-			if (int(chave) > 0):
-				_isint = True
-		except ValueError:
-			_isint = False
-	del(_isint)
-	limpar()
-	while True:
-		inp: str = input("Digite o nome do APP sem caracteres especiais (/h para obter ajuda): ").lower()
-		_comandos = comandos(inp)
-		if(_comandos == 2):
-			break
-		elif(_comandos == 1):
-			continue
-		elif(_comandos == 0):
-			pass
-		else:
-			print("Comando retornou um valor não indexado: ", _comandos)
-			break
-		if(inp.isalpha() == False):
-			continue
-		senha = gen_pass(chave, inp)
-		if senha == -1:
-			print(f"""
---------------------------------------
-Erro ao gerar a senha. Nome do APP e/ou chave inválidos.
-Prssione ENTER para sair
-""")
-			input()
-			break
-		pyperclip.copy(senha)
-		if Show:
-			limpar()
-			print(f"Senha: {senha} ------ (copiada para área de transferência)\n")
-		else:
-			print(f"Senha: {senha[0:5]}*********** - (copiada para área de transferência)\n")
-		del senha
+# Create a button to open an image
+open_button = tk.Button(root, text="Open Image", command=open_image)
+open_button.pack(pady=20)
 
+# Run the application
+root.mainloop()
 
-# ---------------------- Comandos
-def comandos(inp:str):
-	global Show
-	comandos = """
-SHOW  |SHOWPASS |MOSTRAR  |VER |/S ---- Habilita impressão da senha na tela.
-HIDE  |HIDEPASS |ESCONDER          ---- Desabilita a impressão da senha na tela.
-HELP  |AJUDA    |/H                ---- Imprime a tela de ajuda.
-QUIT  |SAIR     |LEAVE    |/Q |/L  ---- Finaliza o programa
-CLEAR |CLS      |LIMPAR   |/C      ---- Limpa a tela
-"""
-	if inp in ("quit", "sair", "leave", "/q", "/l"):
-		if input("Tem certeza? [S/N] ").lower() == "s":
-			print("Saindo...")
-			return 2
-		else:
-			return 1
-	elif(inp in ("mostrar", "show", "ver", "showpass", "/s")):
-		Show = True
-		return 1
-	elif(inp in ("esconder", "hide", "hidepass")):
-		Show = False
-		return 1
-	elif(inp in ("help", "ajuda", "commands", "/h")):
-		print(comandos)
-		return 1
-	elif(inp in ("clear", "limpar", "cls", "/c")):
-		limpar()
-		return 1
-	return 0
-
-
-# ---------------------- Password Generator
-def gen_pass(key: str, appinp: str):
-	try:
-		appname = encrypt_name(key, appinp)
-		senha: str = ""
-		cont = 0
-		for i in range(Size):
-			if int(key[cont]) % 2:
-				isup = True
-			else:
-				isup = False
-			_num = i-1+(ord(appname[i].upper())-65)+int(key[cont])
-			if _num < len(char_list):
-				if i % 2:
-					_char = char_list[_num]
-				else:
-					_char = char_list[-_num]
-			else:
-				if isup:
-					_char = char_list[0]
-				else:
-					_char = char_list[len(char_list)-1]
-			if isup:
-				_char = _char.upper()
-			senha += _char
-			cont = (cont+1 if cont != len(key)-1 else 0)
-		return senha
-	except Exception as e:
-		exc_type, exc_obj, exc_tb = sys.exc_info()
-		fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-		print(exc_type, fname, exc_tb.tb_lineno)
-		return -1
-
-
-
-# ---------------------- App internal name
-def encrypt_name(key, appinp):
-	cur = 1
-	cont = 0
-	_app_name = appinp
-	while(len(_app_name) < Size):
-		if(len(_app_name) > 1):
-			if(len(_app_name)-1 > int(key[cont])):
-				_app_name += char_list[ord(_app_name[int(key[cont])].upper())+cur-65]
-			else:
-				_app_name += char_list[ord(_app_name[(-2)].upper())+cur-65]
-		else:
-			_app_name += _app_name
-		cont = (cont+1 if cont != len(key)-1 else 0)
-		cur += 1
-	return _app_name
-
-
-# ---------------------- CLS
-def limpar():
-	_ = os.system('cls')
-
-
-if(__name__ == '__main__'):
-	__main__()
