851fa8629e22ca577ec83777e9820b5cf5ff8833
Erik Dio <erik.theboy@yahoo.com.br>
2025-01-29 08:10:44 -0300

    Cleaning

--- PasswordManager/main.py
+++ PasswordManager/main.py
@@ -4,6 +4,7 @@ import tkinter as tk
 from tkinter import filedialog
 from tkinter import ttk
 from ttkthemes import ThemedStyle
+import string
 
 # Create the main window
 root = tk.Tk()
@@ -15,19 +16,25 @@ root.geometry("800x600")
 def open_image():
     file_path = filedialog.askopenfilename(filetypes=[("Image Files", "*.png;*.jpg;*.jpeg")])
     sz = 128
-    image = Image.open(file_path).convert("RGB")
-    image.save("conv.png")
-    image = Image.open("conv.png")
+    image = Image.open(file_path)
+    if image.mode != "RGB":
+        image.convert("RGB")
+        image.save(file_path)
+        image = Image.open(file_path)
     offset = int(str(image.width)[0]) + int(str(image.height)[0])
-    #print(image.mode)
     pin = f"{secrets.randbelow(10**128):0128d}"
     print(pin)
-    #print(len(image.getbands()))
     pixel_lst = list(image.getdata())
-    for cursor in range(1, sz+1):        
-        r, g, b = pixel_lst[cursor*offset]
+    for cursor in range(1, sz+1):
+        r=g=b=a=0
+        if(len(image.getbands())==3):
+            r, g, b = pixel_lst[cursor*offset]
+        elif(len(image.getbands())==4):
+            r, g, b, a = pixel_lst[cursor*offset]
+        else:
+            print("Invalid image format")
+            return
         binario = format(int(pin[cursor-1]), '#010b')
-        #print(binario, end=" ")
         redbin = format(r, '#010b')
         greenbin = format(g, '#010b')
         bluebin = format(b, '#010b')
@@ -37,31 +44,36 @@ def open_image():
         x = cursor*offset % image.width
         y = cursor*offset // image.width
         image.putpixel(xy=(x,y), value=(r, g, b))
-        #print(x, y)
     image.save("encrypted.png")
     
 def decrypt():
     file_path = filedialog.askopenfilename(filetypes=[("Image Files", "*.png;*.jpg;*.jpeg")])
     sz = 128
     image = Image.open(file_path)
-    image.convert("RGB")
+    if image.mode != "RGB":
+        image.convert("RGB")
+        image.save(file_path)
+        image = Image.open(file_path)
     offset = int(str(image.width)[0]) + int(str(image.height)[0])
     pixel_lst = list(image.getdata())
     conv = ""
-    #print(len(image.getbands()))
     for cursor in range(1, sz+1):
         r=g=b=a=0
-        r, g, b = pixel_lst[cursor*offset]
+        if(len(image.getbands())==3):
+            r, g, b = pixel_lst[cursor*offset]
+        elif(len(image.getbands())==4):
+            r, g, b, a = pixel_lst[cursor*offset]
+        else:
+            print("Invalid image format")
+            return
         redbin = format(r, '#010b')
         greenbin = format(g, '#010b')
         bluebin = format(b, '#010b')
-        #print(f"{redbin} : {r} {greenbin} : {g} {bluebin} : {b}", end=" | ")
         bincor = f"0b{bluebin[-1]}{greenbin[-1]}{redbin[-2:]}"
-        #print(bincor, end=" ")
         binario = int(bincor, 0)
         conv += str(binario)
     number_label.config(text=conv)
-# Create a button to open an image
+
 open_button = tk.Button(root, text="Open Image", command=open_image)
 decrypt_button = tk.Button(root, text="Decrypt", command=decrypt)
 open_button.pack(pady=20)
---
+++ PasswordManager/param.py
@@ -0,0 +1,9 @@
+import string
+class Config:
+    mode:list = []
+    def __init__(self, mode:list):
+        self.mode = mode
+        if(True):
+            pass
+    def param(self):
+            pass
